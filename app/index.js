e084d55a6721eb8c19f82556d7658b33445d0746
Manav Misra <manavm@visionwebsoft.com>
2023-01-04 10:20:39 -0600

    Use map for array of booleans based upon ages

--- app/index.js
+++ app/index.js
@@ -3,10 +3,24 @@
 // unless we are using a bundler like Webpack or Vite.
 import { isLegal2Drink } from "./lib.js";
 
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 3,
-};
+const persons = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 3,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 21,
+  },
+  {
+    firstName: "Jack",
+    lastName: "Doe",
+    age: 35,
+  },
+];
 
-console.log(isLegal2Drink(person));
+console.log(isLegal2Drink(persons[1]));
+
+const legalDrinkers = persons.map(isLegal2Drink);
